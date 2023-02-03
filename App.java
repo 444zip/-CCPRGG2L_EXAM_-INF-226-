1132b3f65ed67855d68413b53aa2096cd6abea6c
Mikaella Mendoza <122050772+444zip@users.noreply.github.com>
2023-02-03 21:10:20 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,69 @@
+public class App {
+
+    public static void main(String[] args) {
+        
+
+        // Remove letters (30 pts)
+
+        // Instructions (Important!)
+
+        // Without using arrays or loops or regular expressions, apply recursion to remove specific letters in
+        // your full name.
+
+        // 1. If the last letter of your surname is a vowel, remove all vowels in
+        // your full name.
+
+        // 2. If the last letter of your surname is a consonant, remove all
+        // consonants in your full name.
+
+        // 3. Use only lowercase letters
+        
+        // 4. Each letter should be removed one by one.
+
+        // Example:
+        // Full name is "elizer ponio jr".
+        // The output should be "lzr pn jr".
+
+        // Put your complete name in the "fullName" variable.
+        // Example:
+        // String fullName = "elizer ponio jr";
+
+
+                String fullName = "mikaella mendoza";
+        
+                removeLetter(fullName);
+                
+            }
+        
+            static void removeLetter(String str) {
+               
+                extractLetters(str, "");
+        
+            } 
+        
+            static String extractLetters(String str, String result) {
+        
+                if(str.length() == 0) {
+                    return result;
+                }
+                char n = str.charAt(0);
+                switch (n) {
+                
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                  
+                System.out.println(result + str.substring(1));
+                    
+                    return extractLetters(str.substring(1), result);
+                    
+                    default:
+                    
+                    return n + extractLetters(str.substring(1), result + n);
+                }
+            }
+   
+ }
+        
